42516fe91bd5345c2a2732bde5b5466dae8781e7
mauro <mauro@mombelli.com>
2015-07-21 00:34:24 +0200

    fix

--- telegram_status_bot.py
+++ telegram_status_bot.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python -u
 
 from threading import Thread
 import socket
 import sys, requests, json, schedule, time, psutil
+
+from datetime import timedelta
 
 from secret_conf import *
 
@@ -13,6 +15,13 @@ command_read_message = 'getUpdates'
 
 last_update_id = 0
 
+def get_uptime():
+	with open('/proc/uptime', 'r') as f:
+		uptime_seconds = float(f.readline().split()[0])
+		uptime_string = str(timedelta(seconds = uptime_seconds))
+	
+	return uptime_string
+
 def sizeof_fmt(num, suffix='B'):
 	for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
 		if abs(num) < 1024.0:
@@ -21,17 +30,17 @@ def sizeof_fmt(num, suffix='B'):
 	return "%.1f%s%s" % (num, 'Yi', suffix)
 
 def send_message(message, chat_id):
-	data = {'text':'----------\nfrom '+socket.gethostname()+':\n\n'+message+'\n----------\n', 'chat_id':str(chat_id) }
+	data = {'text':'----------\nFROM '+socket.gethostname()+':\n\n'+message+'\n----------\n', 'chat_id':str(chat_id) }
 	r = requests.post(endpoint+'/'+telegram_token+'/'+command_send_message, data=data)
 	return r.json
 
 def send_keep_alive():
 	global chat_id_list
 	for chat_id in chat_id_list:
-		send_message("Buongiorno! Uptime: "+str(psutil.boot_time()), chat_id )
+		send_message("Buongiorno! Uptime: "+get_uptime(), chat_id )
 
 def send_status(chat_id):
-	message = 'UPTIME:\n'+str(psutil.boot_time())
+	message = 'UPTIME:\n'+get_uptime() +'\n'
 	message += "CPU:\n"+str(psutil.cpu_times_percent()) +'\n'
 	message +=  "RAM:\n"+str(psutil.virtual_memory()) +'\n'
 	message +=  'DISK:\n'
@@ -47,11 +56,12 @@ def read_message():
 		data = json.loads(r.text)
 		if 'result' in data:
 			for mess in data['result']:
-				print('messaggio ricevuto da '+str(mess['message']['chat']['id'])+' : '+mess['message']['text'])
+				print('messaggio ricevuto da '+str(mess['message']['chat']['id'])+' : '+ json.dumps(mess['message']) )
 				
 				if mess['message']['chat']['id'] in chat_id_list:
-					if (mess['message']['text'] == '/status'):
-						send_status(mess['message']['chat']['id'])
+					if 'text' in mess['message']:
+						if mess['message']['text'] == '/status' or mess['message']['text'] == '/status@lampone_bot':
+							send_status(mess['message']['chat']['id'])
 				
 				if last_update_id < mess['update_id']:
 					last_update_id = mess['update_id']
