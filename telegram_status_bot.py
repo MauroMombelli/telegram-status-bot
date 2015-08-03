07d9a01ec0b33f20a1f86876e64b362beb1c70a6
Mauro Mombelli <mauro@mombelli.me>
2015-08-03 18:05:38 +0200

    added pacman check

--- telegram_status_bot.py
+++ telegram_status_bot.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python -u
 
-from threading import Thread
-import socket
-import sys, requests, json, schedule, time, psutil
+import schedule, time, psutil
 
 from datetime import timedelta
 
 from secret_conf import *
-
-endpoint = "https://api.telegram.org"
-
-command_send_message = 'sendMessage'
-command_read_message = 'getUpdates'
-
-last_update_id = 0
+from telegram_api import *
+from pacman_status import *
 
 def get_uptime():
 	with open('/proc/uptime', 'r') as f:
@@ -29,15 +22,10 @@ def sizeof_fmt(num, suffix='B'):
 		num /= 1024.0
 	return "%.1f%s%s" % (num, 'Yi', suffix)
 
-def send_message(message, chat_id):
-	data = {'text':'----------\nFROM '+socket.gethostname()+':\n\n'+message+'\n----------\n', 'chat_id':str(chat_id) }
-	r = requests.post(endpoint+'/'+telegram_token+'/'+command_send_message, data=data)
-	return r.json
-
-def send_keep_alive():
+def send_keep_alive(mess = 'Buongiorno'):
 	global chat_id_list
 	for chat_id in chat_id_list:
-		send_message("Buongiorno! Uptime: "+get_uptime(), chat_id )
+		send_message(mess+"! Uptime: "+get_uptime()+"\nAggiornamenti disponibili:\n"+check_update(), chat_id )
 
 def send_status(chat_id):
 	message = 'UPTIME:\n'+get_uptime() +'\n'
@@ -47,27 +35,6 @@ def send_status(chat_id):
 	for partition in psutil.disk_partitions():
 		message += '-  '+ partition.mountpoint +' used: '+ sizeof_fmt(psutil.disk_usage(partition.mountpoint).used)+'/'+sizeof_fmt(psutil.disk_usage(partition.mountpoint).total) +' '+ str(psutil.disk_usage(partition.mountpoint).percent) +'%\n'
 	send_message(message, chat_id)
-
-def read_message():
-	try:
-		global last_update_id, chat_id_list
-		data = {'offset':str(last_update_id+1) }
-		r = requests.get(endpoint+'/'+telegram_token+'/'+command_read_message, data=data)
-		data = json.loads(r.text)
-		if 'result' in data:
-			for mess in data['result']:
-				print('messaggio ricevuto da '+str(mess['message']['chat']['id'])+' : '+ json.dumps(mess['message']) )
-				
-				if mess['message']['chat']['id'] in chat_id_list:
-					if 'text' in mess['message']:
-						if mess['message']['text'] == '/status' or mess['message']['text'] == '/status@lampone_bot':
-							send_status(mess['message']['chat']['id'])
-				
-				if last_update_id < mess['update_id']:
-					last_update_id = mess['update_id']
-	except:
-		print ("Unexpected error:", sys.exc_info()[0])
-		raise
 
 '''
 random_str = ''.join(random.choice( string.ascii_uppercase + string.digits ) for _ in range(16))
@@ -80,8 +47,10 @@ print ( set_web_hook('lampone.mooo.com', random_str, 8443) )
 #self signed cert are NOT supported
 #unset_web_hook()
 
-for chat_id in chat_id_list:
-	send_message('bot avviato', chat_id)
+#for chat_id in chat_id_list:
+#	send_message('bot avviato', chat_id)
+
+send_keep_alive('Bot avviato')
 
 schedule.every().day.at("9:30").do( send_keep_alive )
 
